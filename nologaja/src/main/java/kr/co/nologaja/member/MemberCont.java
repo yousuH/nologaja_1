6f5a0763c4be68ed90d776d9332044a2e6645bed
조씨 <choyumin01@gmail.com>
2021-01-07 11:07:59 +0900

    memberform 완료

--- nologaja/src/main/java/kr/co/nologaja/member/MemberCont.java
+++ nologaja/src/main/java/kr/co/nologaja/member/MemberCont.java
@@ -19,336 +19,333 @@ import jdk.jfr.MetadataDefinition;
 import kr.co.nologaja.member.BuyerDTO;
 import net.utility.mail;
 
-
 @Controller
 public class MemberCont {
-	
+
 	@Inject
 	BuyerDAO bdao;
-	
+
 	@Inject
 	SellerDAO sdao;
-	
+
 	@Inject
 	mail mailSending;
-	
+
 	@Inject
 	mail mailSending2;
-	
+
 	public MemberCont() {
 		System.out.println("==MemberCont()==");
 	}
-	
-	//회원가입 페이지(배너 상의 회원가입 버튼)
+
+	// 회원가입 페이지(배너 상의 회원가입 버튼)
 	@RequestMapping(value = "/memberform.do")
-	   public ModelAndView form() {
-	      ModelAndView mav = new ModelAndView();
-	      mav.setViewName("member/memberform");
-	      return mav;
-	   }
-	
-	//회원가입 구매자 dto
+	public ModelAndView form() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("member/memberform");
+		return mav;
+	}
+
+	// 회원가입 구매자 dto
 	@RequestMapping("/buyerjoin.do")
 	public String insert(@ModelAttribute BuyerDTO dto) {
 		bdao.insert(dto);
 		return "member/login";
-	}//insert() end
+	}// insert() end
 
-	//구매자 아이디 체크
+	// 구매자 아이디 체크
 	@RequestMapping("/buyerProc.do")
 	public void buyerCheckProc(HttpServletRequest req, HttpServletResponse resp) {
 		try {
-			String uid=req.getParameter("uid");
-			int cnt=bdao.duplecateID(uid);
-			
-			JSONObject json=new JSONObject();
-			//key.put(key, value)
+			String uid = req.getParameter("uid");
+			int cnt = bdao.duplecateID(uid);
+
+			JSONObject json = new JSONObject();
+			// key.put(key, value)
 			json.put("count", cnt);
 			resp.setContentType("text/plain; charset=UTF-8");
-			PrintWriter out=resp.getWriter();
+			PrintWriter out = resp.getWriter();
 			out.println(json.toString());
 			out.flush();
 			out.close();
-			
-		}catch (Exception e) {
-			System.out.println("아이디 중복확인 쿠키 실패:"+e);
-		}//end
-	}//buyerProc() end
-	
-	//구매자 이메일 체크
+
+		} catch (Exception e) {
+			System.out.println("아이디 중복확인 쿠키 실패:" + e);
+		} // end
+	}// buyerProc() end
+
+	// 구매자 이메일 체크
 	@RequestMapping("/buyerEmailProc.do")
 	public void buyerEmailCheckProc(HttpServletRequest req, HttpServletResponse resp) {
 		try {
-			String uemail=req.getParameter("uemail");
-			int cnt=bdao.duplecateEmail(uemail);
-			
-			JSONObject json=new JSONObject();
-			//key.put(key, value)
+			String uemail = req.getParameter("uemail");
+			int cnt = bdao.duplecateEmail(uemail);
+
+			JSONObject json = new JSONObject();
+			// key.put(key, value)
 			json.put("count", cnt);
 			resp.setContentType("text/plain; charset=UTF-8");
-			PrintWriter out=resp.getWriter();
+			PrintWriter out = resp.getWriter();
 			out.println(json.toString());
 			out.flush();
 			out.close();
-			
-		}catch (Exception e) {
-			System.out.println("이메일 중복확인 쿠키 실패:"+e);
-		}//end
-	}//buyerEmailCheckProc() end
 
-	
-	//회원가입 판매자 dto
+		} catch (Exception e) {
+			System.out.println("이메일 중복확인 쿠키 실패:" + e);
+		} // end
+	}// buyerEmailCheckProc() end
+
+	// 회원가입 판매자 dto
 	@RequestMapping("/sellerjoin.do")
 	public String insert(@ModelAttribute SellerDTO dto) {
 		sdao.insert(dto);
 		return "member/login";
-	}//insert() end
-	
-	//판매자 아이디 체크
+	}// insert() end
+
+	// 판매자 아이디 체크
 	@RequestMapping("/sellerProc.do")
 	public void sellerCheckProc(HttpServletRequest req, HttpServletResponse resp) {
 		try {
-			String suid=req.getParameter("suid");
-			int cnt=bdao.duplecateID(suid);
-			
-			JSONObject json=new JSONObject();
-			//key.put(key, value)
+			String suid = req.getParameter("suid");
+			int cnt = sdao.duplecateID(suid);
+
+			JSONObject json = new JSONObject();
+			// key.put(key, value)
 			json.put("count", cnt);
 			resp.setContentType("text/plain; charset=UTF-8");
-			PrintWriter out=resp.getWriter();
+			PrintWriter out = resp.getWriter();
 			out.println(json.toString());
 			out.flush();
 			out.close();
-			
-		}catch (Exception e) {
-			System.out.println("아이디 중복확인 쿠키 실패:"+e);
-		}//end
-	}//sellerProc() end
-	
-	//판매자 이메일 체크
+
+		} catch (Exception e) {
+			System.out.println("아이디 중복확인 쿠키 실패:" + e);
+		} // end
+	}// sellerProc() end
+
+	// 판매자 이메일 체크
 	@RequestMapping("/sellerEmailProc.do")
 	public void sellerEmailCheckProc(HttpServletRequest req, HttpServletResponse resp) {
 		try {
-			String uemail=req.getParameter("uemail");
-			int cnt=sdao.duplecateEmail(uemail);
-				
-			JSONObject json=new JSONObject();
-			//key.put(key, value)
+			String uemail = req.getParameter("uemail");
+			int cnt = sdao.duplecateEmail(uemail);
+
+			JSONObject json = new JSONObject();
+			// key.put(key, value)
 			json.put("count", cnt);
 			resp.setContentType("text/plain; charset=UTF-8");
-			PrintWriter out=resp.getWriter();
+			PrintWriter out = resp.getWriter();
 			out.println(json.toString());
 			out.flush();
 			out.close();
-				
-		}catch (Exception e) {
-			System.out.println("이메일 중복확인 쿠키 실패:"+e);
-		}//end
-	}//sellerEmailCheckProc() end
+
+		} catch (Exception e) {
+			System.out.println("이메일 중복확인 쿠키 실패:" + e);
+		} // end
+	}// sellerEmailCheckProc() end
 
 //---------------------------------------------------------------------------------------------------------------------------------------------
-	//로그인페이지로 이동
-		@RequestMapping("/login.do")
-		   public ModelAndView login() {
-		      ModelAndView mav = new ModelAndView();
-		      mav.setViewName("member/login");
-		      return mav;
-	   }
-		
+	// 로그인페이지로 이동
+	@RequestMapping("/login.do")
+	public ModelAndView login() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("member/login");
+		return mav;
+	}
 
-		//구매자 로그인처리
-		@RequestMapping("/blogin.do")
-		public ModelAndView blogin(String uid, String upw, HttpServletRequest request, HttpServletResponse resp) throws Exception {
-			boolean result = bdao.blogin(uid, upw);
+	// 구매자 로그인처리
+	@RequestMapping("/blogin.do")
+	public ModelAndView blogin(String uid, String upw, HttpServletRequest request, HttpServletResponse resp)
+			throws Exception {
+		boolean result = bdao.blogin(uid, upw);
+		ModelAndView mav = new ModelAndView();
+		if (result == true) {
+			String ugrd = bdao.read_bgrd(uid, upw);
+			mav.setViewName("index");
+			HttpSession session = request.getSession();
+			session.setAttribute("uid", uid);
+			session.setAttribute("ugrd", ugrd);
+			session.setMaxInactiveInterval(20 * 60 * 24);
+		} else {
+			resp.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = resp.getWriter();
+			out.println("<script>alert('로그인 정보를 확인해주세요.'); history.go(-1);</script>");
+			out.flush();
+			mav.setViewName("member/login");
+		}
+		return mav;
+	}// blogin() end
+
+	// 판매자 로그인처리
+	@RequestMapping("/slogin.do")
+	public ModelAndView slogin(String suid, String supw, HttpServletRequest request, HttpServletResponse resp)
+			throws Exception {
+		boolean result = sdao.slogin(suid, supw);
+		ModelAndView mav = new ModelAndView();
+		if (result == true) {
+			String ugrd = sdao.read_sgrd(suid, supw);
+			mav.setViewName("index");
+			HttpSession session = request.getSession();
+			session.setAttribute("suid", suid);
+			session.setAttribute("ugrd", ugrd);
+			session.setMaxInactiveInterval(20 * 60 * 24);
+		} else {
+			resp.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = resp.getWriter();
+			out.println("<script>alert('로그인 정보를 확인해주세요.'); history.go(-1);</script>");
+			out.flush();
+			mav.setViewName("member/login");
+		}
+		return mav;
+	}// slogin() end
+
+	// 로그아웃
+	@RequestMapping("/logout.do")
+	public ModelAndView logout(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		session.removeAttribute("suid");
+		session.removeAttribute("uid");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("member/login");
+		return mav;
+	}
+
+	// 회원정보수정 폼 불러오기
+	@RequestMapping(value = "/updateform.do")
+	public ModelAndView update(HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		String ugrd = (String) session.getAttribute("ugrd");
+		System.out.println(ugrd);
+		// B1은 판매회원, C1는 일반회원
+		if (ugrd.equals("B1")) {
 			ModelAndView mav = new ModelAndView();
-			if(result==true) {
-				String ugrd=bdao.read_bgrd(uid, upw);
-				mav.setViewName("index");
-				HttpSession session=request.getSession();
-				session.setAttribute("uid", uid);
-				session.setAttribute("ugrd", ugrd);
-				session.setMaxInactiveInterval(20*60*24);
-			}else {
-		        resp.setContentType("text/html; charset=UTF-8");
-		        PrintWriter out = resp.getWriter();
-		        out.println("<script>alert('로그인 정보를 확인해주세요.'); history.go(-1);</script>");
-		        out.flush();
-				mav.setViewName("member/login");
-			}
+			mav.setViewName("member/s_updateform");
+			String suid = (String) session.getAttribute("suid");
+			mav.addObject("dto", sdao.detail(suid));
+			mav.addObject("ugrd", ugrd);
 			return mav;
-		}//blogin() end
-		
-		
-		//판매자 로그인처리
-		@RequestMapping("/slogin.do")
-		public ModelAndView slogin(String suid, String supw, HttpServletRequest request, HttpServletResponse resp) throws Exception {
-			boolean result = sdao.slogin(suid, supw);
+		} else if (ugrd.equals("C1")) {
 			ModelAndView mav = new ModelAndView();
-			if(result==true) {
-				String ugrd=sdao.read_sgrd(suid, supw);
-				mav.setViewName("index");
-				HttpSession session=request.getSession();
-				session.setAttribute("suid", suid);
-				session.setAttribute("ugrd", ugrd);
-				session.setMaxInactiveInterval(20*60*24);
-			}else {
-		        resp.setContentType("text/html; charset=UTF-8");
-		        PrintWriter out = resp.getWriter();
-		        out.println("<script>alert('로그인 정보를 확인해주세요.'); history.go(-1);</script>");
-		        out.flush();
-				mav.setViewName("member/login");
-			}
+			mav.setViewName("member/b_updateform");
+			String uid = (String) session.getAttribute("uid");
+			mav.addObject("dto", bdao.detail(uid));
+			mav.addObject("ugrd", ugrd);
 			return mav;
-		}//slogin() end
-		
-		//로그아웃
-		@RequestMapping("/logout.do")
-	    public ModelAndView logout(HttpServletRequest request) {
-			HttpSession session=request.getSession();
-			session.removeAttribute("suid");
-			session.removeAttribute("uid");
+		}
+		return null;
+	}
+
+	// 회원정보수정하기
+	@RequestMapping(value = "/updateproc.do")
+	public ModelAndView update(@ModelAttribute BuyerDTO bdto, SellerDTO sdto, HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		String ugrd = (String) session.getAttribute("ugrd");
+		// B1은 판매회원, C1는 일반회원
+		if (ugrd.equals("B1")) {
 			ModelAndView mav = new ModelAndView();
-		    mav.setViewName("member/login");
-		    return mav;
+			mav.setViewName("index");
+			sdao.update(sdto);
+			return mav;
+		} else if (ugrd.equals("C1")) {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("index");
+			bdao.update(bdto);
+			return mav;
 		}
-		//회원정보수정 폼 불러오기
-		@RequestMapping(value = "/updateform.do")
-		public ModelAndView update(HttpServletRequest req) {
-			HttpSession session = req.getSession();
-			String ugrd = (String)session.getAttribute("ugrd");
-			System.out.println(ugrd);
-		//B1은 판매회원, C1는 일반회원
-			if(ugrd.equals("B1")) {
-				ModelAndView mav = new ModelAndView();
-				mav.setViewName("member/s_updateform");
-				String suid = (String)session.getAttribute("suid");
-				mav.addObject("dto", sdao.detail(suid));
-				mav.addObject("ugrd",ugrd);
-				return mav;
-			} else if(ugrd.equals("C1")) {
-				ModelAndView mav = new ModelAndView();
-				mav.setViewName("member/b_updateform");
-				String uid = (String)session.getAttribute("uid");
-				mav.addObject("dto", bdao.detail(uid));
-				mav.addObject("ugrd",ugrd);
-				return mav;
-			}
-			return null;
-		}
-		
-		//회원정보수정하기
-		@RequestMapping(value = "/updateproc.do")
-		public ModelAndView update(@ModelAttribute BuyerDTO bdto, SellerDTO sdto, HttpServletRequest req) {
-			HttpSession session = req.getSession();
-			String ugrd = (String)session.getAttribute("ugrd");
-		//B1은 판매회원, C1는 일반회원
-			if(ugrd.equals("B1")) {
-				ModelAndView mav = new ModelAndView();
-				mav.setViewName("index");
-				sdao.update(sdto);
-				return mav;
-			} else if(ugrd.equals("C1")) {
-				ModelAndView mav = new ModelAndView();
-				mav.setViewName("index");
-				bdao.update(bdto);
-				return mav;
-			}
-			return null;
-		}
-	
+		return null;
+	}
+
 //------------------------------------------------------------------------------------------------------------------------------
 
-		
-		// 아이디 비밀번호 찾기 페이지
-		@RequestMapping(value = "/findidpw.do")
-		public ModelAndView findidpw() {
-			ModelAndView mav = new ModelAndView();
-			mav.setViewName("member/findidpw");
-			return mav;
+	// 아이디 비밀번호 찾기 페이지
+	@RequestMapping(value = "/findidpw.do")
+	public ModelAndView findidpw() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("member/findidpw");
+		return mav;
+	}
+
+	// 아이디 비밀번호 찾기 결과(일반회원)
+	@RequestMapping(value = "/buyerfindid.do")
+	public ModelAndView buyerfindid(@ModelAttribute BuyerDTO dto) {
+		ModelAndView mav = new ModelAndView();
+		BuyerDTO result = bdao.bfindid(dto);
+
+		if (result == null) {
+			mav.addObject("msg2", "<p>일치하는 계정을 찾을 수 없습니다.</p>");
+			mav.setViewName("member/findidpwresult");
+		} else {
+			mailSending.mailSending(dto, result);
+
+			mav.addObject("msg1", "<table><tr><th>이름</th><td>" + dto.getUname() + "</td></tr><tr><th>이메일</th><td>"
+					+ dto.getUemail() + "</td></tr></table>");
+			mav.addObject("msg2", "<p>" + dto.getUemail() + "로 아이디와 비밀번호를 전송하였습니다.</p>");
+			mav.setViewName("member/findidpwresult");
+			mav.addObject("dto", dto);
 		}
-		
+		return mav;
+	}
 
-		// 아이디 비밀번호 찾기 결과(일반회원)
-		@RequestMapping(value = "/buyerfindid.do")
-		public ModelAndView buyerfindid(@ModelAttribute BuyerDTO dto) {
-		      ModelAndView mav = new ModelAndView();
-		      BuyerDTO result = bdao.bfindid(dto);
-		      
-		      if(result==null) {
-		    	  mav.addObject("msg2", "<p>일치하는 계정을 찾을 수 없습니다.</p>");
-		    	  mav.setViewName("member/findidpwresult");
-		      }else { 
-		    	  mailSending.mailSending(dto, result);
-		    	  
-		    	  mav.addObject("msg1", "<table><tr><th>이름</th><td>"+dto.getUname()+"</td></tr><tr><th>이메일</th><td>"+dto.getUemail()+"</td></tr></table>");
-		    	  mav.addObject("msg2", "<p>"+ dto.getUemail() +"로 아이디와 비밀번호를 전송하였습니다.</p>");
-		    	  mav.setViewName("member/findidpwresult");
-		    	  mav.addObject("dto", dto);
-		      }
-		      return mav;
+	// 아이디 비밀번호 찾기 결과(판매회원)
+	@RequestMapping(value = "/sellerfindid.do")
+	public ModelAndView sellerfindid(@ModelAttribute SellerDTO dto) {
+		ModelAndView mav = new ModelAndView();
+		SellerDTO result = sdao.sfindid(dto);
+
+		if (result == null) {
+			mav.addObject("msg2", "<p>일치하는 계정을 찾을 수 없습니다.</p>");
+			mav.setViewName("member/findidpwresult");
+		} else {
+			mailSending.mailSending2(dto, result);
+
+			mav.addObject("msg1", "<table><tr><th>이름</th><td>" + dto.getUname() + "</td></tr><tr><th>이메일</th><td>"
+					+ dto.getUemail() + "</td></tr></table>");
+			mav.addObject("msg2", "<p>" + dto.getUemail() + "로 아이디와 비밀번호를 전송하였습니다.</p>");
+			mav.setViewName("member/findidpwresult");
+			mav.addObject("dto", dto);
 		}
-		
-		
-		// 아이디 비밀번호 찾기 결과(판매회원)
-		@RequestMapping(value = "/sellerfindid.do")
-		public ModelAndView sellerfindid(@ModelAttribute SellerDTO dto) {
-			ModelAndView mav = new ModelAndView();
-			SellerDTO result = sdao.sfindid(dto);
+		return mav;
+	}
 
-			if (result == null) {
-				mav.addObject("msg2", "<p>일치하는 계정을 찾을 수 없습니다.</p>");
-				mav.setViewName("member/findidpwresult");
-			} else {
-				mailSending.mailSending2(dto, result);
+	// 회원탈퇴페이지로 이동
+	@RequestMapping("/delete.do")
+	public ModelAndView delete() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mypage/memDelForm");
+		return mav;
+	}
 
-				mav.addObject("msg1", "<table><tr><th>이름</th><td>" + dto.getUname() + "</td></tr><tr><th>이메일</th><td>"
-						+ dto.getUemail() + "</td></tr></table>");
-				mav.addObject("msg2", "<p>" + dto.getUemail() + "로 아이디와 비밀번호를 전송하였습니다.</p>");
-				mav.setViewName("member/findidpwresult");
-				mav.addObject("dto", dto);
-			}
-			return mav;
-		}
+	// 구매자 회원 탈퇴
+	@RequestMapping("/bdelete.do")
+	public ModelAndView bdelete(HttpSession session) {
 
-		// 회원탈퇴페이지로 이동
-		@RequestMapping("/delete.do")
-		public ModelAndView delete() {
-			ModelAndView mav = new ModelAndView();
-			mav.setViewName("mypage/memDelForm");
-			return mav;
-		}
-		
-		// 구매자 회원 탈퇴
-		@RequestMapping("/bdelete.do")
-		public ModelAndView bdelete(HttpSession session) {
+		// 세션영역에 있는 세션변수값 가져오기
+		// session.setAttribute("uid", uid);
+		String uid = (String) session.getAttribute("uid");
+		bdao.bdelete(uid);
 
-			// 세션영역에 있는 세션변수값 가져오기
-			// session.setAttribute("uid", uid);
-			String uid = (String) session.getAttribute("uid");
-			bdao.bdelete(uid);
+		// 회원탈퇴가 성공되었다면...세션에 있는 모든 변수가 삭제
+		session.removeAttribute("uid");
+		session.removeAttribute("ugrd");
+		session.setMaxInactiveInterval(0);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		return mav;
+	}// ndelete() end
 
-			// 회원탈퇴가 성공되었다면...세션에 있는 모든 변수가 삭제
-			session.removeAttribute("uid");
-			session.removeAttribute("ugrd");
-			session.setMaxInactiveInterval(0);
-			ModelAndView mav = new ModelAndView();
-			mav.setViewName("index");
-			return mav;
-		}// ndelete() end
+	// 판매자 회원탈퇴
+	@RequestMapping("/sdelete.do")
+	public ModelAndView sdelete(HttpSession session) {
 
-		// 판매자 회원탈퇴
-		@RequestMapping("/sdelete.do")
-		public ModelAndView sdelete(HttpSession session) {
+		String suid = (String)session.getAttribute("suid");
+		sdao.sdelete(suid);
 
-			String suid = (String) session.getAttribute("suid");
-			sdao.sdelete(suid);
+		session.removeAttribute("suid");
+		session.removeAttribute("ugrd");
+		session.setMaxInactiveInterval(0);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		return mav;
+	}// sdelete() end
 
-			session.removeAttribute("suid");
-			session.removeAttribute("ugrd");
-			session.setMaxInactiveInterval(0);
-			ModelAndView mav = new ModelAndView();
-			mav.setViewName("index");
-			return mav;
-		}// sdelete() end
-
-}//class end
+}// class end
--- nologaja/src/main/webapp/resources/js/myscript.js
+++ nologaja/src/main/webapp/resources/js/myscript.js
@@ -6,51 +6,51 @@
 /************* 구매자 아이디&이메일 중복 확인 시작 *************/
 //아이디
 $(function(){
-	$.removeCookie("checkID");
+	$.removeCookie("b_checkID");
 });
 	    	
 $("#btn_uid").click(function(){
 	var uid= "uid=" + $("#uid").val().trim();
-	$.post("buyerProc.do", uid, checkID, "json");
+	$.post("buyerProc.do", uid, b_checkID, "json");
 });//click end
 
-function checkID(result){
+function b_checkID(result){
 	var count=eval(result.count); //형변환
 	if(count==0){
 		$("#panel_uid").css("color", "blue");
 		$("#panel_uid").text("사용가능한 아이디입니다");
 		//$.cookie("쿠키변수명", 값)
-		$.cookie("checkID", "PASS");
+		$.cookie("b_checkID", "PASS");
 	}else {
 		$("#panel_uid").css("color", "red");
 		$("#panel_uid").text("중복된 아이디입니다");
 		$("#uid").focus(); //커서생성
 	}//if end
-}//checkID() end
+}//b_checkID() end
 
 //이메일
 $(function(){
-	$.removeCookie("checkEmail");
+	$.removeCookie("b_checkEmail");
 });
 	    	
-$("#btn_uemail").click(function(){
+$("#b_btn_uemail").click(function(){
 	var uemail= "uemail=" + $("#uemail").val().trim();
-	$.post("buyerEmailProc.do", uemail, checkEmail, "json");
+	$.post("buyerEmailProc.do", uemail, b_checkEmail, "json");
 });//click end
 
-function checkEmail(result){
+function b_checkEmail(result){
 	var count=eval(result.count); //형변환
 	if(count==0){
-		$("#panel_uemail").css("color", "blue");
-		$("#panel_uemail").text("사용가능한 이메일입니다");
+		$("#b_panel_uemail").css("color", "blue");
+		$("#b_panel_uemail").text("사용가능한 이메일입니다");
 		//$.cookie("쿠키변수명", 값)
-		$.cookie("checkEmail", "PASS");
+		$.cookie("b_checkEmail", "PASS");
 	}else {
-		$("#panel_uemail").css("color", "red");
-		$("#panel_uemail").text("중복된 이메일입니다");
+		$("#b_panel_uemail").css("color", "red");
+		$("#b_panel_uemail").text("중복된 이메일입니다");
 		$("#uemail").focus(); //커서생성
 	}//if end
-}//checkEmail() end
+}//b_checkEmail() end
 	    	
 function b_send(){
 
@@ -74,8 +74,8 @@ function b_send(){
 
 //--------------------------------------------------------------  
 	//아이디 쿠키변수값 가져오기
-	var checkID=$.cookie("checkID");
-	if(checkID=="PASS"){
+	var b_checkID=$.cookie("b_checkID");
+	if(b_checkID=="PASS"){
 		return true;
 	}else {
 		$("#panel_uid").css("color", "green");
@@ -85,12 +85,12 @@ function b_send(){
 	}//if end
 	
 	//이메일 쿠키변수값 가져오기
-	var checkEmail=$.cookie("checkEmail");
-	if(checkEmail=="PASS"){
+	var b_checkEmail=$.cookie("b_checkEmail");
+	if(b_checkEmail=="PASS"){
 		return true;
 	}else {
-		$("#panel_uemail").css("color", "green");
-		$("#panel_uemail").text("이메일 중복확인 해주세요");
+		$("#b_panel_uemail").css("color", "green");
+		$("#b_panel_uemail").text("이메일 중복확인 해주세요");
 		$("#uemail").focus(); //커서생성
 		return false;
 	}//if end
@@ -99,46 +99,151 @@ function b_send(){
 /************* 구매자 아이디&이메일 중복 확인 끝 *************/
 
 
-/************* 구매자 비밀번호 일치 여부 시작 *************/
+/************* 판매자 아이디&이메일 중복 확인 시작 *************/
+//아이디
 $(function(){
-	$("#alert-success").hide(); //원문의 div 문장 숨김
-	$("#alert-danger").hide();
+	$.removeCookie("s_checkID");
+});
+	    	
+$("#btn_suid").click(function(){
+	var suid= "suid=" + $("#suid").val().trim();
+	$.post("sellerProc.do", suid, s_checkID, "json");
+});//click end
+
+function s_checkID(result){
+	var count=eval(result.count); //형변환
+	if(count==0){
+		$("#panel_suid").css("color", "blue");
+		$("#panel_suid").text("사용가능한 아이디입니다");
+		//$.cookie("쿠키변수명", 값)
+		$.cookie("s_checkID", "PASS");
+	}else {
+		$("#panel_suid").css("color", "red");
+		$("#panel_suid").text("중복된 아이디입니다");
+		$("#suid").focus(); //커서생성
+	}//if end
+}//s_checkID() end
+
+//이메일
+$(function(){
+	$.removeCookie("s_checkEmail");
+});
+	    	
+$("#s_btn_uemail").click(function(){
+	var uemail= "uemail=" + $("#uemail").val().trim();
+	$.post("sellerEmailProc.do", uemail, s_checkEmail, "json");
+});//click end
+
+function s_checkEmail(result){
+	var count=eval(result.count); //형변환
+	if(count==0){
+		$("#s_panel_uemail").css("color", "blue");
+		$("#s_panel_uemail").text("사용가능한 이메일입니다");
+		//$.cookie("쿠키변수명", 값)
+		$.cookie("s_checkEmail", "PASS");
+	}else {
+		$("#s_panel_uemail").css("color", "red");
+		$("#s_panel_uemail").text("중복된 이메일입니다");
+		$("#uemail").focus(); //커서생성
+	}//if end
+}//s_checkEmail() end
+	    	
+function s_send(){
+
+	//아이디 5~10글자 인지?
+	var suid=document.getElementById("suid").value;
+	suid=suid.trim();
+	if(!(suid.length>=5 && suid.length<=10)){
+		alert("아이디 5~10글자 이내 입력해 주세요");
+		document.getElementById("suid").focus();
+		return false;
+  	}//if end
+  	
+	//비밀번호 5~10글자 인지?
+	var supw=document.getElementById("supw").value;
+	supw=supw.trim();
+	if(!(supw.length>=5 && supw.length<=10)){
+		alert("비밀번호 5~10글자 이내 입력해 주세요");
+		document.getElementById("supw").focus();
+		return false;
+	}//if end
+
+//--------------------------------------------------------------  
+	//아이디 쿠키변수값 가져오기
+	var s_checkID=$.cookie("s_checkID");
+	if(s_checkID=="PASS"){
+		return true;
+	}else {
+		$("#panel_suid").css("color", "green");
+		$("#panel_suid").text("아이디 중복확인 해주세요");
+		$("#suid").focus(); //커서생성
+		return false;
+	}//if end
+	
+	//이메일 쿠키변수값 가져오기
+	var s_checkEmail=$.cookie("s_checkEmail");
+	if(s_checkEmail=="PASS"){
+		return true;
+	}else {
+		$("#s_panel_uemail").css("color", "green");
+		$("#s_panel_uemail").text("이메일 중복확인 해주세요");
+		$("#uemail").focus(); //커서생성
+		return false;
+	}//if end
+}//s_send() end
+
+/************* 판매자 아이디&이메일 중복 확인 끝 *************/
+
+
+/************* 구매자&판매자 비밀번호 일치 여부 시작 *************/
+$(function(){
+	
+	$("#b_alert-success").hide(); //원문의 div 문장 숨김
+	$("#b_alert-danger").hide();
+	
 	$("input").keyup(function(){
 	
 		var upw=$("#upw").val();
 		var reupw=$("#reupw").val();
 		
-		var supw=$("#supw").val();
-		var sreupw=$("#sreupw").val();
-		
-		if(upw != null){
-			if(upw != "" || reupw != ""){ //두가지 속성 비교
-				if(upw == reupw){
-					$("#alert-success").show();
-					$("#alert-danger").hide();
-					$("#submit").removeAttr("disabled");
-				}else{
-					$("#alert-success").hide();
-					$("#alert-danger").show();
-					$("#submit").attr("disabled", "disabled");
-				}
-			}
-		}else if(supw != null) {
-			if(supw != "" || sreupw != ""){ //두가지 속성 비교
-				if(supw == sreupw){
-					$("#alert-success").show();
-					$("#alert-danger").hide();
-					$("#submit").removeAttr("disabled");
-				}else{
-					$("#alert-success").hide();
-					$("#alert-danger").show();
-					$("#submit").attr("disabled", "disabled");
-				}
+		if(upw != "" || reupw != ""){ //두가지 속성 비교
+			if(upw == reupw){
+				$("#b_alert-success").show();
+				$("#b_alert-danger").hide();
+				$("#submit").removeAttr("disabled");
+			}else{
+				$("#b_alert-success").hide();
+				$("#b_alert-danger").show();
+				$("#submit").attr("disabled", "disabled");
 			}
 		}
 	});
 });
-/************* 구매자 비밀번호 일치 여부 끝 *************/
+/************* 구매자&판매자 비밀번호 일치 여부 끝 *************/
 
 
-        
+/************* 구매자&판매자 비밀번호 일치 여부 시작 *************/
+$(function(){
+	
+	$("#s_alert-success").hide(); //원문의 div 문장 숨김
+	$("#s_alert-danger").hide();
+	
+	$("input").keyup(function(){
+	
+		var supw=$("#supw").val();
+		var sreupw=$("#sreupw").val();
+		
+		if(supw != "" || sreupw != ""){
+			if(supw == sreupw){
+				$("#s_alert-success").show();
+				$("#s_alert-danger").hide();
+				$("#submit").removeAttr("disabled");
+			}else{
+				$("#s_alert-success").hide();
+				$("#s_alert-danger").show();
+				$("#submit").attr("disabled", "disabled");
+			}
+		}
+	});
+});
+/************* 구매자&판매자 비밀번호 일치 여부 끝 *************/
